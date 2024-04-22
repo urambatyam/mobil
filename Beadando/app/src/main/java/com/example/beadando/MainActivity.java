6bcb955922e2d4488dad05ed454d3c771f755a42
h270519 <h270519@stud.u-szeged.hu>
2024-04-22 13:27:41 +0200

    elso proba

---
+++ Beadando/app/src/main/java/com/example/beadando/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.beadando;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
+
+    EditText userET;
+
+    EditText codeET;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        userET = findViewById(R.id.editTextUsername);
+        codeET = findViewById(R.id.editTextPassword);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void login(View view) {
+        String struser = userET.getText().toString();
+        String strcode = codeET.getText().toString();
+
+        Log.i(LOG_TAG, "felhasználó: "+struser+" jelszó: "+strcode);
+    }
+}
